e0b03cc46093e72460b883a8b07036edac43015f
Iffannn <111673314+Iffannn@users.noreply.github.com>
2023-03-22 14:01:11 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,4 +24,15 @@ def call_name(name: str):
 def call_name(name: str):
     return {"hello": name}
 
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World1"}
+
+def test_callname():
+    name = "Iffanz"
+    response = client.post("/callname", json={"name": name})
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
+
 handler = Mangum(app)
