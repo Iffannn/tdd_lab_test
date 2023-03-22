1178b27667e2c222a31ca0f7d416a82ccb816add
Iffannn <111673314+Iffannn@users.noreply.github.com>
2023-03-22 13:42:25 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -4,8 +4,19 @@ from app.main import app
 
 client = TestClient(app)
 
-
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World1"}
+
+def test_callname():
+    name = "Iffan"
+    response = client.get(f"/callname/{name}")
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
+
+def test_callname():
+    name = "Iffanz"
+    response = client.post("/callname", json={"name": name})
+    assert response.status_code == 200
+    assert response.json() == {"hello": name}
